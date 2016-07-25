0951343e6e48976e520dd6088ae544c924b74c59
Christopher Ong <chrisongts@gmail.com>
2016-07-25 21:11:12 +0800

    Added static pages home about help

--- app/controllers/statis_pages_controller.rb
+++
@@ -1,2 +0,0 @@
-class StatisPagesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :posts
   root 'application#welcome'  # controller#method
   resources :users
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
